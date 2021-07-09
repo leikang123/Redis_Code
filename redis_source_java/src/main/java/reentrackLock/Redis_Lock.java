1e11d45de6578b80a6aca4ebb1e1a4e0bacb5c9d
leikang123 <mac1094@localhost>
2021-07-09 18:04:11 +0800

    update

---
+++ redis_source_java/src/main/java/reentrackLock/Redis_Lock.java
@@ -0,0 +1,35 @@
+package reentrackLock;
+
+import java.util.Map;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * redis分布式锁
+ * @author mac1094
+ *
+ */
+public class Redis_Lock {
+	// 创建分布式锁
+	private ThreadLocal<Map<String,Integer>> lockers = new ThreadLocal<>();
+	private Jedis jedis;
+	
+	public Redis_Lock(Jedis jedis) {
+		this.jedis = jedis;
+	}
+	private boolean _lock(String key) {
+		return jedis.set(key,"","ex")!=null;
+	}
+    private void _unlock(String key) {
+    	jedis.del(key);
+    }
+    
+    private Map<String,Integer> currentLockers(){
+    	Map<String,Integer> refs = lockers.get();
+    	if(refs !=null) {
+    		return refs;
+    	}
+    }
+    lockers.set(new HashMap<>());
+    
+}
